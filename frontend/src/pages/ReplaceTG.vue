98e351729d902b8633f9261bb681d5f18d8f57dc
nariman079 <nariman079i@mail.ru>
2024-11-13 11:39:59 +0300

    new dist for frontend

---
+++ frontend/src/pages/ReplaceTG.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        404
+    </div>
+</template>
+<script>
+export default {
+    mounted (){
+        window.location.href = 'https://web.telegram.org/k/#@WallPrankBot'
+    }
+}
+</script>
+<style>
+    
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import MainPage from '@/pages/MainPage.vue'
 import SuccessPage from './pages/SuccessPage.vue'
+import ReplaceTG from './pages/ReplaceTG.vue'
 
 const routes = [
+  {name: 'replacetg', path: '/', component: ReplaceTG},
   { name: 'main', path: '/:telegram_id', component: MainPage  },
   { name:'success_page', path:'/p/success', component: SuccessPage }
 ]
