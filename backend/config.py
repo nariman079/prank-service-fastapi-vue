8a6d9735166992123388b5c5f725333d132b52e0
nariman079 <nariman079i@mail.ru>
2024-11-15 17:57:21 +0300

    fix error

--- backend/config.py
+++ backend/config.py
@@ -35,9 +35,7 @@ class DBAction:
         """
         Создание документа
         """
-        return DBController(
-            cls.__name__
-        ).table.insert_one(
+        return DBController(cls.__name__).table.insert_one(
             cls(**kwargs).dict()
         )
 
