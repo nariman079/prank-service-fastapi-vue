24ae60df2e4338b861f6afbd9f49d68900f83c3d
nariman079 <nariman079i@mail.ru>
2024-11-16 15:26:55 +0300

    settings log

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ logging.getLogger().handlers[0].setFormatter(ColoredFormatter())
 last_chunk_time = dict()
 active_tasks = dict()
 
-INACTIVITY_TIMEOUT = 1-0.5
+INACTIVITY_TIMEOUT = 2
 
 app = FastAPI()
 
