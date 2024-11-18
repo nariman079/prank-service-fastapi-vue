d2850a51105a4c72aacbb32d319699b0da74021a
nariman079 <nariman079i@mail.ru>
2024-11-18 05:06:07 +0300

    added favicon

---
+++ ston_frontend/src/components/ReplaceTG.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    mounted (){
+        window.location.href = "https://web.telegram.org/k/#@WallPrankBot"
+    }
+}
+</script>
+<style>
+    
+</style>
--- ston_frontend/src/router.js
+++ ston_frontend/src/router.js
@@ -1,8 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import MainPage from './MainPage.vue'
+import ReplaceTG from './components/ReplaceTG.vue'
 
 
 const routes = [
+  {path: '/', component: ReplaceTG},
   {  path: '/:telegram_hash', component: MainPage },
 ]
 
